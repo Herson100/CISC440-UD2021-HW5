2313b8c70ea3f993e773b467528a0866480b0b17
Herson100 <36927204+Herson100@users.noreply.github.com>
2021-05-15 19:25:21 -0400

    Added starter project
    Code from HW3 to start with - largely provided by my professor with a few tweaks from myself for HW3

---
+++ HW5/Predator.hh
@@ -0,0 +1,64 @@
+#ifndef PREDATOR_HH
+
+#define PREDATOR_HH
+
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+//
+// "Creature Box" -- flocking app
+//
+// by Christopher Rasmussen
+//
+// CISC 440/640, March, 2014
+// updated to OpenGL 3.3, March, 2016
+//
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+
+#include "Creature.hh"
+#include "Flocker.hh"
+
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+
+#define MAX_PREDATOR_SPEED                           0.04
+
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+
+class Predator : public Creature
+{
+public:
+
+	glm::vec3 hunger_force;
+
+	double random_force_limit;
+
+	double hunger_weight;
+	double min_hunger_distance, min_squared_hunger_distance;
+	double max_hunger_distance, max_squared_hunger_distance;
+	double inv_range_squared_hunger_distance;
+  
+  Predator(int,                    // index
+	   double, double, double, // initial position
+	   double, double, double, // initial velocity
+	   double,				   // random uniform acceleration limit
+	   float, float, float,    // base color
+	   double, double, double, // min, max hunger distance, weight
+	   int = 1);               // number of past states to save
+
+  void draw(glm::mat4);
+  void update();
+  bool compute_hunger_force();
+
+
+
+};
+
+//----------------------------------------------------------------------------
+
+void calculate_predator_flocker_squared_distances();
+
+//----------------------------------------------------------------------------
+
+#endif
